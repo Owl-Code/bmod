2fde49690f0207ea5a29147bb25df2dc033524b8
Owl-Code <Paul.einwich@gmail.com>
2018-05-25 23:28:37 -0700

    Bees
    They almost here, but crash atm

--- components/trapping/bee_hive_component.lua
+++ components/trapping/bee_hive_component.lua
@@ -1,1 +1,22 @@
-bee_hive = class()
+local BeeHiveComponent = class()
+
+function BeeHiveComponent:can_trap(traget)
+  local json = radiant.entities.get_json(self)
+  local bee_hive = json.bee_hive or false
+
+  local data = radiant.entities.get_entity_data(target, 'stonehearth:bait_trap')
+  local can_trap = data and data.can_trap
+  local can_hive = data.can_hive
+  if not can_trap or (not can_hive and bee_hive) then
+     return false
+  end
+
+
+  -- check for any other dynamic conditions here
+  -- specifically, don't trap animals that are pets of the trapper's town
+  local player_id = radiant.entities.get_player_id(target)
+  return player_id == 'critters'
+
+end
+
+return BeeHiveComponent
